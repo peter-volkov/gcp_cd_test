8e821d2e4edc942e223ae68c2afb70292bb34a92
peevo <peevo@yandex-team.ru>
2022-10-04 14:25:03 +0300

    commit 4

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello 10 {name}!"
+    return "Hello 11 {name}!"
 
 
 if __name__ == "__main__":
